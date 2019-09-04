28c62ff087f3e64da30705233e5c84ae74e803f7
andresm07 <33080733+andresm07@users.noreply.github.com>
2019-09-04 01:47:22 -0600

    AM3

--- src/components/Form/form.js
+++ src/components/Form/form.js
@@ -6,6 +6,7 @@
  */
 import React from 'react';
 import './styles.scss'
+import {Button} from 'semantic-ui-react';
 
 class Form extends React.Component {
   constructor(props) {
@@ -17,10 +18,38 @@ class Form extends React.Component {
   render() {
     return (
       <div className="form">
-        <div className="form__TextoNombre">
-            tyhtyujt
+        <p className="form__contactenos">
+          Contáctenos
+        </p>
+        <div className="form__datos">
+          <div className="form__datos__labels">
+            <p className="form__datos__labels__tag">
+              Nombre completo (*)
+            </p>
+            <p className="form__datos__labels__tag">
+              Correo electrónico (*)
+            </p>
+            <p className="form__datos__labels__tag">
+              Teléfono (*)
+            </p>
+            <p className="form__datos__labels__tag">
+              Comentarios (*)
+            </p>
+          </div>
+          <div className="form__datos__inputs">
+            <input className="form__datos__inputs__info" type="text" name="nombre"/>
+            <input className="form__datos__inputs__info" type="email" name="correo"/>
+            <input className="form__datos__inputs__info" type="tel" name="telefono" pattern="[6-8]{1}[0-9]{3}-[0-9]{4}"/>
+            <textarea className="form__datos__inputs__textarea" name="comentarios"/>
+          </div>
         </div>
-      </div>
+        <p className="form__nota">
+          Nota: Los campos con (*) son obligatorios
+        </p>
+        <Button className="form__enviar">
+          Enviar
+        </Button>
+    </div>
     )
   }
 }
